398451fe98b89e521364de92e873e5596b72e3fb
ReleeSquirrel <relee_s@hotmail.com>
2025-07-11 02:06:00 -0400

    Format output

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import word_count
+from stats import count_words
+from stats import count_each_character
+from stats import sort_character_dictionary
 
 def get_book_text(filepath):
     result = ""
@@ -8,8 +10,23 @@ def get_book_text(filepath):
 
 
 def main():
-    count_of_words = word_count(get_book_text("books/frankenstein.txt"))
-    print(f"{count_of_words} words found in the document")
+    book_location = "books/frankenstein.txt"
+    book_text = get_book_text(book_location)
+    count_of_words = count_words(book_text)
+    count_of_characters = count_each_character(book_text)
+    sorted_character_list = sort_character_dictionary(count_of_characters)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_location}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_of_words} total words")
+    print("--------- Character Count -------")
+    
+    for character in sorted_character_list:
+        if character["character"].isalpha():
+            print(f"{character["character"]}: {character["count"]}")
+    
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,26 @@
-def word_count(text):
+def count_words(text):
     words = text.split()
     return len(words)
+
+
+def count_each_character(text):
+    lower_case_text = text.lower()
+    result = {}
+    for character in lower_case_text:
+        if character in result:
+            result[character] += 1
+        else:
+            result[character] = 1
+    return result
+
+
+def sort_on_character_count(character):
+    return character["count"]
+
+
+def sort_character_dictionary(character_dictionary):
+    sorted_list = []
+    for character in character_dictionary:
+        sorted_list.append({"character": character, "count": character_dictionary[character]})
+    sorted_list.sort(reverse=True, key=sort_on_character_count)
+    return sorted_list
